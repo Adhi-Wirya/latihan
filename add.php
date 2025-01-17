58f64840fc78f8171765785ce45aec6bc3c6e791
Adhi-Wirya <cihuysheesh@gmail.com>
2025-01-17 08:21:04 +0700

    add.php

--- add.php
+++ add.php
@@ -4,34 +4,38 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
  
-<body>
-	<a href="index.php">Go to Home</a>
-	<br/><br/>
- 
-	<form action="add.php" method="post" name="form1">
-		<table width="25%" border="0">
-			<tr> 
-				<td>Name</td>
-				<td><input type="text" name="name"></td>
-			</tr>
-			<tr> 
-				<td>Email</td>
-				<td><input type="text" name="email"></td>
-			</tr>
-			<tr> 
-				<td>Mobile</td>
-				<td><input type="text" name="mobile"></td>
-			</tr>
-			<tr>
-            <td>Foto</td>
-			<td><input type="file" name="foto" id="foto"></td>
-</tr> 
-				<td></td>
-				<td><input type="submit" name="Submit" value="Add" class="btn btn-outline-primary"></td>
-			</tr>
-		</table>
-	</form>
+	<body>
+		<a href="index.php">Go to Home</a>
+		<br/><br/>
 	
+		<div class="href-target" id="input-types">
+		<h1>Add New User Form</h1>
+		
+		<form action="add.php" method="post" name="form1">
+			<table width="25%" border="0">
+				<tr> 
+					<td>Name</td>
+					<td><input type="text" name="name"></td>
+				</tr>
+				<tr> 
+					<td>Email</td>
+					<td><input type="text" name="email"></td>
+				</tr>
+				<tr> 
+					<td>Mobile</td>
+					<td><input type="text" name="mobile"></td>
+				</tr>
+				<tr>
+				<td>Foto</td>
+				<td><input type="file" name="foto" id="foto"></td>
+	            </tr> 
+					<td></td>
+					<td><input type="submit" name="Submit" value="Add" class="btn btn-outline-primary"></td>
+				</tr>
+			</table>
+		</form>
+	</div>	
+
 	<?php
  
 	// Check If form submitted, insert form data into users table.
